766a18c1de9a3cb724c151e5cba7fed7f3a91930
Jinol <jinolshah08@gmail.com>
2021-05-06 23:38:27 +0530

    Additional feature and exe file

--- source_code/sudosolver.py
+++ source_code/sudosolver.py
@@ -1,9 +1,17 @@
 from tkinter import *
 from solver import *
 from mover import *
+from version import *
+import webbrowser
 
 root = Tk()
 root.title("SudoSolver")
+
+root.maxsize(378, 441)
+root.minsize(378, 441)
+
+def linker(event):
+    webbrowser.open_new_tab(repolink)
 
 def binder(ele, entry):
     ele.bind('<Down>', lambda event, enter=entry: downer(event, enter))
@@ -59,15 +67,25 @@ for i in entry:
     for j in i:
         binder(j['obj'], entry)
 
-solve = Button(text='Solve', width=14, command=lambda:solver(entry, solve))
+solve = Button(text='Solve', cursor="hand2", width=14, command=lambda:solver(entry, solve))
 solve.grid(column=3, row=10, columnspan=3, pady=6)
 
-clear = Button(text='Clear', width=14, command=lambda:clearer(entry, solve))
+clear = Button(text='Clear', cursor="hand2", width=14, command=lambda:clearer(entry, solve))
 clear.grid(column=0, row = 10, columnspan=3, pady=6)
 
-exitter = Button(text='Exit', width=14, command=root.quit)
+exitter = Button(text='Exit', width=14, command=root.quit, cursor="hand2")
 exitter.grid(column=6, row=10, columnspan=3, pady=6)
 
+author = Label(text='by Jinol Shah', fg='#696969', font='Helvetica 9 italic', cursor="hand2")
+author.grid(row=11, column=6, columnspan=3, padx=(30,0), pady=(0,4))
+author.bind("<ButtonRelease-1>", linker)
 
+version = Label(text=version, fg='#696969', font='Helvetica 9 italic', cursor="hand2")
+version.grid(row=11, column=0, columnspan=1, padx=(2,0), pady=(0,4))
+version.bind("<ButtonRelease-1>", linker)
+
+# root.update()
+# print(root.winfo_height())
+# print(root.winfo_width())
 
 root.mainloop()
