a592f63d7eec719c1353a6a62d41e0753bbc1ed9
Jinol <jinolshah08@gmail.com>
2021-05-06 15:52:10 +0530

    Added solver

--- solver.py
+++ solver.py
@@ -1,92 +1,101 @@
-def blacker(entry):
-    for i in range(9):
-        for j in range(9):
-            entry[i][j]['fg'] = 'black'
-            entry[i][j]['obj'].config(fg='black')
+from checker import *
+from pprint import pprint
+import copy
 
-
-def rowChecker(entry, i):
+def row(unsudoku, k, i, j):
     inval = False
-    for j in range(8):
-        if entry[i][j]['obj'].get():
-            for y in range(j+1, 9):
-                if (entry[i][j]['obj'].get() == entry[i][y]['obj'].get()):
-                    entry[i][j]['fg'] = '#B00020'
-                    entry[i][j]['obj'].config(fg='#B00020')
-                    entry[i][y]['fg'] = '#B00020'
-                    entry[i][y]['obj'].config(fg='#B00020')
-                    inval = True
+    for x in range(9):
+        if x == j:
+            continue
+        if k == unsudoku[i][x]:
+            inval = True
     if inval:
         return False
     else:
         return True
 
-                
-def columnChecker(entry, j):
+def column(unsudoku, k, i, j):
     inval = False
-    for i in range(8):
-        if entry[i][j]['obj'].get(): 
-            for y in range(i+1, 9):
-                if (entry[i][j]['obj'].get() == entry[y][j]['obj'].get()):
-                    entry[i][j]['fg'] = '#B00020'
-                    entry[i][j]['obj'].config(fg='#B00020')
-                    entry[y][j]['fg'] = '#B00020'
-                    entry[y][j]['obj'].config(fg='#B00020')
-                    inval = True
+    for y in range(9):
+        if y == i:
+            continue
+        if k == unsudoku[y][j]:
+            inval = True
     if inval:
         return False
     else:
         return True
 
-def boxChecker(entry, x):
+def box(unsudoku, k, i, j):
     inval = False
-    a = ((x//3)*3)
-    b = ((x%3)*3)
-    for i in range(a,a+3):
-        for j in range(b,b+3):
-            if i==a+2 and j==b+2:
+    a = ((i//3)*3)
+    b = ((j//3)*3)
+    for x in range(a, a+3):
+        for y in range(b, b+3):
+            if x == i and y == j:
                 continue
-            if entry[i][j]['obj'].get(): 
-                for y in range(i, a+3):
-                    for z in range(b, b+3):
-                        if y==i and z==j:
-                            continue
-                        if (entry[i][j]['obj'].get() == entry[y][z]['obj'].get()):
-                            entry[i][j]['fg'] = '#B00020'
-                            entry[i][j]['obj'].config(fg='#B00020')
-                            entry[y][z]['fg'] = '#B00020'
-                            entry[y][z]['obj'].config(fg='#B00020')
-                            inval = True
+            if k == unsudoku[x][y]:
+                inval = True
     if inval:
         return False
     else:
         return True
 
+def checked(unsudoku, k, i, j):
+    check1 = row(unsudoku, k, i, j)
+    if check1:
+        check2 = column(unsudoku, k, i, j)
+        if check2:
+            check3 = box(unsudoku, k, i, j)
+            if check3:
+                return True
+            else: return False
+        else: return False
+    else: return False
 
-def checker(entry):
-    blacker(entry)
-    rowVal = True
-    colVal = True
-    boxVal = True
-    for i in range(9):
-        rowVal = rowVal and rowChecker(entry, i)
-        colVal = colVal and columnChecker(entry, i)
-        boxVal = boxVal and boxChecker(entry, i)
-    if rowVal and colVal and boxVal:
-        return True
-    else:
-        return False
-
-def solution(entry):
+def matrixgen(entry):
     sudoku = []
     for i in range(9):
         sublst = []
         for j in range(9):
-            sublst.append(entry[i][j]['obj'].get())
+            if entry[i][j]['obj'].get():
+                sublst.append(int(entry[i][j]['obj'].get()))
+            else:
+                sublst.append(0)
         sudoku.append(sublst)
-    print(sudoku)
+    return sudoku
+
+def finder(unsudoku):
+    for i in range(9):
+        for j in range(9):
+            if not unsudoku[i][j]:
+                return i, j
+    return None, None
+
+def solution(unsudoku):
+    i, j = finder(unsudoku)
+    if i is None and j is None:
+        return True
+    for k in range(1,10):
+        flag = checked(unsudoku, k, i, j)
+        if flag:
+            unsudoku[i][j] = k
+            if solution(unsudoku):
+                return True
+            else:
+                continue
+    unsudoku[i][j] = 0
+    return False
 
 def solver(entry):
     entValid = checker(entry)
     if entValid:
-        solution(entry)
+        sudoku = matrixgen(entry)
+        unsudoku = copy.deepcopy(sudoku)
+        if solution(unsudoku):
+            pprint(unsudoku)
+            for i in range(9):
+                for j in range(9):
+                    if not sudoku[i][j]:
+                        entry[i][j]['obj'].config(fg='#5E8C77')
+                        entry[i][j]['obj'].insert(0, unsudoku[i][j])
